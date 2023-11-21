2c1fb8ee0bc63bf1b0c9dbaef1d86e10a1178cb3
HKEDU <HKEDU@DESKTOP-563A44B>
2023-11-21 11:27:55 +0900

    프로젝트 생성

---
+++ hkdelivery/src/main/java/hkdelivery/HkdeliveryApplication.java
@@ -0,0 +1,13 @@
+package hkdelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HkdeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HkdeliveryApplication.class, args);
+	}
+
+}
---
+++ hkdelivery/src/test/java/hkdelivery/HkdeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package hkdelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HkdeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
